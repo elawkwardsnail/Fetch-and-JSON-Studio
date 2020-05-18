d0512b8dfdd08a1118f138a0210fb1df8b8665fd
Daniel Ramirez <danny@Daniels-MacBook-Pro.local>
2020-05-17 23:30:46 -0400

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // TODO: add code here
+let json = [];
+let astronautCode = '';
+
+window.addEventListener("load", function() {
+    const fetchPromise = fetch("https://handlers.education.launchcode.org/static/astronauts.json");
+    fetchPromise.then(function(response) {
+        response.json().then(function(json) {
+            console.log(json[1].skills);
+
+            const container = document.getElementById("container");
+
+            for (let i in json) {
+                astronautCode = astronautCode.concat(`
+            <div class="astronaut">
+                <div class="bio">
+                    <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${json[i].hoursInSpace}</li>
+                        <li>Active: ${json[i].active}</li>
+                        <li>Skills: ${json[i].skills}</li>
+                    </ul>
+                </div>
+                <img class="avatar" src="images/${json[i].picture}">
+            </div>
+            <br>`
+                );
+            };
+            console.log(astronautCode);
+
+            container.innerHTML = astronautCode;
+        });
+    });
+});
